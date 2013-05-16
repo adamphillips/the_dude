75c17682153cfe3364a6af4b3bad1e1cacae3257
Adam Phillips <aphillips@scholastic.co.uk>
2013-05-16 01:18:19 +0100

    Fixed issue with loading duderc
    The Dsl methods were not available from the duderc
    
    Also an alias to TheDude.say has been added

--- lib/the_dude/dsl.rb
+++ lib/the_dude/dsl.rb
@@ -10,5 +10,19 @@ module TheDude
       TheDude::Command.new *args, &block
     end
 
+    # Forwards the call on to {TheDude.say}
+    def say *args
+      TheDude.say *args
+    end
+
+    # Class methods
+    class << self
+      # Reads in a file and processes it using The Dude Dsl
+      #
+      # @param [String] path Path to the file to read
+      def from_file path
+        new.instance_eval File.read(path) if File.exists? path
+      end
+    end
   end
 end
